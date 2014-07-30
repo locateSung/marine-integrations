e475a1bcfd0bb7a20a65571bb3bcc13d5f1b59d8
sahn <locateSung@gmail.com>
2014-07-30 12:17:21 -0700

    Initial check-in

--- mi/instrument/KML/particles.py
+++ mi/instrument/KML/particles.py
@@ -28,6 +28,22 @@ from mi.core.instrument.data_particle import CommonDataParticleType
 # Particle Regex's'
 #
 CAMDS_RESPONSE = r'.*\n<::>'
+CAMDS_RESPONSE_MATCHER = re.compile(CAMDS_RESPONSE,re.DOTALL)
+
+CAMDS_VIDEO = r'.{500}'
+CAMDS_VIDEO_MATCH = re.compile(CAMDS_VIDEO, re.DOTALL)
+
+# ADCP_PD0_PARSED_REGEX = r'\x7f\x7f(..)'  # .*
+# ADCP_PD0_PARSED_REGEX_MATCHER = re.compile(ADCP_PD0_PARSED_REGEX, re.DOTALL)
+# ADCP_SYSTEM_CONFIGURATION_REGEX = r'(Instrument S/N.*?)\>'
+# ADCP_SYSTEM_CONFIGURATION_REGEX_MATCHER = re.compile(ADCP_SYSTEM_CONFIGURATION_REGEX, re.DOTALL)
+# ADCP_COMPASS_CALIBRATION_REGEX = r'(ACTIVE FLUXGATE CALIBRATION MATRICES in NVRAM.*?)\>'
+# ADCP_COMPASS_CALIBRATION_REGEX_MATCHER = re.compile(ADCP_COMPASS_CALIBRATION_REGEX, re.DOTALL)
+# ADCP_ANCILLARY_SYSTEM_DATA_REGEX = r'(Ambient  Temperature.*\n.*\n.*)\n>'
+# ADCP_ANCILLARY_SYSTEM_DATA_REGEX_MATCHER = re.compile(ADCP_ANCILLARY_SYSTEM_DATA_REGEX)
+# ADCP_TRANSMIT_PATH_REGEX = r'(IXMT.*\n.*\n.*\n.*)\n>'
+# ADCP_TRANSMIT_PATH_REGEX_MATCHER = re.compile(ADCP_TRANSMIT_PATH_REGEX)
+
 
 # ##############################################################################
 # Data Particles
@@ -39,14 +55,34 @@ class DataParticleType(BaseEnum):
     RAW = CommonDataParticleType.RAW
     CAMDS_RESPONSE = "camds_response"
     CAMDS_VIDEO_BINARY = "camds_video_binary"
+    CAMDS_HEALTH_STATUS = "camds_health_status"
+    CAMDS_DISK_STATUS = "camds_disc_status"
 
 # keys for video stream
 class CAMDS_VIDEO_KEY(BaseEnum):
-    CAMDS_VIDEO = "camds video"
+    CAMDS_VIDEO_BINARY = "camds_video_binary"
 
 # Data particle for PT4 command
 class CAMDS_VIDEO(DataParticle):
     _data_particle_type = DataParticleType.CAMDS_VIDEO_BINARY
+
+    def _build_parsed_values(self):
+
+        result = []
+        result.append({DataParticleKey.VALUE_ID: CAMDS_VIDEO_KEY.CAMDS_VIDEO_BINARY,
+                       DataParticleKey.VALUE: self.raw_data})
+        return result
+
+
+# HS command
+class CAMDS_HEALTH_STATUS_KEY(BaseEnum):
+    temp = "camds_temp"
+    humidity = "camds_humidity"
+    error = "camds_error"
+
+# Data particle for HS command
+class CAMDS_HEALTH_STATUS(DataParticle):
+    _data_particle_type = DataParticleType.CAMDS_HEALTH_STATUS
 
     RE01 = re.compile(r'.*')
 
@@ -54,7 +90,7 @@ class CAMDS_VIDEO(DataParticle):
         # Initialize
         matches = {}
         for key, regex, formatter in [
-            (CAMDS_VIDEO_KEY.CAMDS_VIDEO, self.RE01, float)
+            (CAMDS_HEALTH_STATUS_KEY.temp, self.RE01, float)
         ]:
             match = regex.search(self.raw_data)
             matches[key] = formatter(match.group(1))
@@ -66,6 +102,35 @@ class CAMDS_VIDEO(DataParticle):
 
         return result
 
+
+# GC command
+class CAMDS_DISK_STATUS_KEY(BaseEnum):
+    size = "camds_disc_size"
+    disk_remaining = "camds_disc_remaining"
+    image_remaining = "camds_images_remaining"
+    image_on_disk = "camds_images_on_disc"
+
+# Data particle for GC command
+class CAMDS_DISK_STATUS(DataParticle):
+    _data_particle_type = DataParticleType.CAMDS_HEALTH_STATUS
+
+    RE01 = re.compile(r'.*')
+
+    def _build_parsed_values(self):
+        # Initialize
+        matches = {}
+        for key, regex, formatter in [
+            (CAMDS_DISK_STATUS_KEY.image_on_disk, self.RE01, float)
+        ]:
+            match = regex.search(self.raw_data)
+            matches[key] = formatter(match.group(1))
+
+        result = []
+        for key, value in matches.iteritems():
+            result.append({DataParticleKey.VALUE_ID: key,
+                           DataParticleKey.VALUE: value})
+
+        return result
 
 
 
